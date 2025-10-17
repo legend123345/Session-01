6fe1c0cb6569bd4db120c1e8f708f8e1658c46b3
legend123345 <venbadhanush@gmail.com>
2025-10-17 09:39:17 +0530

    My second file added

---
+++ script.py
@@ -0,0 +1,7 @@
+# A simple Python program
+def greet(name):
+    print(f"Hello, {name}! Welcome to Python + Git Bash demo.")
+
+if __name__ == "__main__":
+    user = input("Enter your name: ")
+    greet(user)
